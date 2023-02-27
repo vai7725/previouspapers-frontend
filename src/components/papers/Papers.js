776f798bdde232ff6d67831c9765653a7efb7dd7
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2023-02-28 00:02:39 +0530

    filter sidebar added to papers page

--- src/components/papers/Papers.js
+++ src/components/papers/Papers.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useParams } from "react-router";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { GlobalContext } from "../../context/Context";
 import PaperCard from "./PaperCard";
 import Loading from "../Loading";
@@ -11,178 +12,212 @@ const Papers = () => {
   const { university } = useParams();
 
   const {
-    backendURL,
     papersData,
-    loading,
-    papersFiltered,
-    arePapersFiltered,
-    showAlert,
-    alertMsg,
+    backendURL,
+    courseNameArr,
+    courseYearArr,
+    paperTitleArr,
+    paperYearArr,
+    showFilterSidebar,
   } = state;
+
+  // console.log(papersData);
 
   const fetchPapers = async () => {
     dispatch({ type: "HANDLE_LOADING", payload: true });
     const res = await axios.get(`${backendURL}/api/papers/${university}`);
     const { paperData } = await res.data;
+    // making set of courseName
+    const courseNameSet = Array.from(
+      new Set(
+        await paperData.map((item) => {
+          return item.courseName;
+        })
+      )
+    );
+
+    // making set of courseYear
+    const courseYearSet = Array.from(
+      new Set(
+        await paperData.map((item) => {
+          return item.courseYear;
+        })
+      )
+    );
+
+    // making set of paperTitle
+    const paperTitleSet = Array.from(
+      new Set(
+        await paperData.map((item) => {
+          return item.paperTitle;
+        })
+      )
+    );
+
+    // making set of paperTitle
+    const paperYearSet = Array.from(
+      new Set(
+        await paperData.map((item) => {
+          return item.paperYear;
+        })
+      )
+    );
+
+    const filterItemObj = {
+      courseNameArr: courseNameSet,
+      courseYearArr: courseYearSet,
+      paperTitleArr: paperTitleSet,
+      paperYearArr: paperYearSet,
+    };
+
     if (paperData) {
       dispatch({ type: "UPDATE_PAPERS_DATA", payload: paperData });
+      dispatch({ type: "SET_FILTER_ITEMS", payload: filterItemObj });
     } else {
       dispatch({ type: "UPDATE_PAPERS_DATA", payload: [] });
     }
   };
 
-  const papers = papersData.map((paper) => {
-    return <PaperCard key={paper._id} {...paper} />;
-  });
-
-  const filteredPapers = papersFiltered.map((paper) => {
-    return <PaperCard key={paper._id} {...paper} />;
-  });
-
-  // Dynamic courseName option generating code
-  const courseNameListArr = papersData.map((paper, i) => {
-    return paper.courseName;
-  });
-
-  const courseNameList = Array.from(
-    new Set(["--Select Course--", ...courseNameListArr])
-  );
-
-  const courseNameOptions = courseNameList.map((item) => {
+  // adding courseName to the sidebar respective field dynamically
+  const courseNameFilterList = courseNameArr.map((item) => {
     return (
-      <option key={crypto.randomUUID()} value={item} className="filter-option">
+      <label
+        key={crypto.randomUUID()}
+        htmlFor={`'${item}'`}
+        className="content"
+      >
         {item}
-      </option>
+        <input
+          type="checkbox"
+          name={item}
+          id={`'${item}'`}
+          className="filter-checkbox"
+        />
+      </label>
     );
   });
 
-  // Dynamic courseYear option generating code
-  const courseYearListArr = papersData.map((paper, i) => {
-    return paper.courseYear;
-  });
-
-  const courseYearList = Array.from(
-    new Set(["--Select Course Year--", ...courseYearListArr])
-  );
-
-  const courseYearOptions = courseYearList.map((item) => {
+  // adding courseYear to the sidebar respective field dynamically
+  const courseYearFilterList = courseYearArr.map((item) => {
     return (
-      <option key={crypto.randomUUID()} value={item} className="filter-option">
+      <label
+        key={crypto.randomUUID()}
+        htmlFor={`'${item}'`}
+        className="content"
+      >
         {item}
-      </option>
+        <input
+          type="checkbox"
+          name={item}
+          id={`'${item}'`}
+          className="filter-checkbox"
+        />
+      </label>
     );
   });
 
-  // Dynamic subject option generating code
-  const subjectListArr = papersData.map((paper, i) => {
-    return paper.paperTitle;
-  });
-
-  const subjectList = Array.from(
-    new Set(["--Select Subject--", ...subjectListArr])
-  );
-
-  const subjectOptions = subjectList.map((item) => {
+  // adding subjects to the sidebar respective field dynamically
+  const subjectsFilterList = paperTitleArr.map((item) => {
     return (
-      <option key={crypto.randomUUID()} value={item} className="filter-option">
+      <label
+        key={crypto.randomUUID()}
+        htmlFor={`'${item}'`}
+        className="content"
+      >
         {item}
-      </option>
+        <input
+          type="checkbox"
+          name={item}
+          id={`'${item}'`}
+          className="filter-checkbox"
+        />
+      </label>
     );
   });
 
-  // Dynamic paperYear option generating code
-  const paperYearListArr = papersData.map((paper, i) => {
-    return paper.paperYear;
-  });
-
-  const paperYearList = Array.from(
-    new Set(["--Select Paper Year--", ...paperYearListArr])
-  );
-
-  const paperYearOptions = paperYearList.map((item) => {
+  // adding paperYear to the sidebar respective field dynamically
+  const paperYearFilterList = paperYearArr.map((item) => {
     return (
-      <option key={crypto.randomUUID()} value={item} className="filter-option">
+      <label
+        key={crypto.randomUUID()}
+        htmlFor={`'${item}'`}
+        className="content"
+      >
         {item}
-      </option>
+        <input
+          type="checkbox"
+          name={item}
+          id={`'${item}'`}
+          className="filter-checkbox"
+        />
+      </label>
     );
   });
-
-  const closeAlert = () => {
-    setTimeout(() => {
-      dispatch({ type: "CLOSE_ALERT" });
-    }, 3000);
-  };
-
-  useEffect(() => {
-    showAlert && alert(`${alertMsg}`);
-  }, [showAlert]);
 
   useEffect(() => {
     fetchPapers();
   }, []);
 
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
     <section className="papers-section">
-      <Alert closeAlert={closeAlert} />
-      <header className="filter-section">
-        <select
-          name="courseName"
-          id=""
-          className="filter"
-          onChange={(e) => {
-            console.log("Changed");
-            dispatch({
-              type: "FILTER_PAPERS_BY_COURSE_NAME",
-              payload: e.target.value[0] == "-" ? "" : e.target.value,
-            });
-          }}
-        >
-          {courseNameOptions}
-        </select>
-        <select
-          name="courseYear"
-          id=""
-          className="filter"
-          onChange={(e) => {
-            dispatch({
-              type: "FILTER_PAPERS_BY_COURSE_YEAR",
-              payload: e.target.value[0] == "-" ? "" : e.target.value,
-            });
-          }}
-        >
-          {courseYearOptions}
-        </select>
-        <select
-          name="subject"
-          id=""
-          className="filter"
-          onChange={(e) =>
-            dispatch({
-              type: "FILTER_PAPERS_BY_SUBJECT",
-              payload: e.target.value[0] == "-" ? "" : e.target.value,
-            })
-          }
-        >
-          {subjectOptions}
-        </select>
-        <select name="" id="" className="filter">
-          {paperYearOptions}
-        </select>
-      </header>
-      {arePapersFiltered && (
-        <button
-          className="btn clearFilter-btn"
-          onClick={() => dispatch({ type: "CLEAR_PAPER_FILTERS" })}
-        >
-          Clear filters
-        </button>
-      )}
+      <button
+        className="btn toggleSidebar-btn"
+        onClick={() =>
+          dispatch({ type: "HIDE_FILTER_SIDEBAR", payload: !showFilterSidebar })
+        }
+      >
+        {window.innerWidth > "576" ? <FaBars /> : " Filter papers"}
+      </button>
+      <aside
+        className={`${
+          showFilterSidebar
+            ? "filter-sidebar filter-sidebar-show"
+            : "filter-sidebar"
+        }`}
+      >
+        <header>
+          <h1>Filter papers</h1>
+          <button
+            className="btn sidebar-btn"
+            onClick={() =>
+              dispatch({
+                type: "HIDE_FILTER_SIDEBAR",
+                payload: !showFilterSidebar,
+              })
+            }
+          >
+            <FaTimes />
+          </button>
+        </header>
+        <main className="filter-content">
+          <section className="courseName-section filter-sidebar-section">
+            <h3>Courses:</h3>
+            {courseNameFilterList}
+          </section>
+          <section className="courseYear-section filter-sidebar-section">
+            <h3>Course Year:</h3>
+            {courseYearFilterList}
+          </section>
+          <section className="subject-section filter-sidebar-section">
+            <h3>Subjects:</h3>
+            {subjectsFilterList}
+          </section>
+          <section className="paperYear-section filter-sidebar-section">
+            <h3>Paper Years:</h3>
+            {paperYearFilterList}
+          </section>
+        </main>
+        <footer className="sidebar-action-btns">
+          <button className="btn sidebar-action-btn clearFilter-btn">
+            Clear filters
+          </button>
+          <button className="btn sidebar-action-btn applyFilter-btn">
+            Apply filters
+          </button>
+        </footer>
+      </aside>
       <div className="papers-box">
-        {papersFiltered.length < 1 ? papers : filteredPapers}
+        {/* {papersFiltered.length < 1 ? papers : filteredPapers} */}
       </div>
     </section>
   );
--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -104,53 +104,61 @@ export const reducer = (state, action) => {
     };
   }
 
-  if (type === "FILTER_PAPERS_BY_COURSE_NAME") {
-    const filteredPaperArr = state.papersData.filter((paper) =>
-      payload === "" ? paper : paper.courseName === payload
-    );
+  if (type === "SET_FILTER_ITEMS") {
+    // console.log("payload ", payload);
     return {
       ...state,
-      papersFiltered: filteredPaperArr,
-      arePapersFiltered: true,
+      ...payload,
     };
   }
 
-  if (type === "FILTER_PAPERS_BY_COURSE_YEAR") {
-    if (state.papersFiltered.length < 1) {
-      return {
-        ...state,
-        showAlert: true,
-        alertMsg: "Select the course first!",
-      };
-    } else {
-      const filteredPaperArr = state.papersFiltered.filter((paper) =>
-        payload === "" ? paper : paper.courseYear === payload
-      );
-      return {
-        ...state,
-        papersFiltered: filteredPaperArr,
-        arePapersFiltered: true,
-      };
-    }
-  }
-  if (type === "FILTER_PAPERS_BY_COURSE_YEAR") {
-    if (state.papersFiltered.length < 1) {
-      return {
-        ...state,
-        showAlert: true,
-        alertMsg: "Select the course first!",
-      };
-    } else {
-      const filteredPaperArr = state.papersFiltered.filter((paper) =>
-        payload === "" ? paper : paper.courseYear === payload
-      );
-      return {
-        ...state,
-        papersFiltered: filteredPaperArr,
-        arePapersFiltered: true,
-      };
-    }
-  }
+  // if (type === "FILTER_PAPERS_BY_COURSE_NAME") {
+  //   const filteredPaperArr = state.papersData.filter((paper) =>
+  //     payload === "" ? paper : paper.courseName === payload
+  //   );
+  //   return {
+  //     ...state,
+  //     papersFiltered: filteredPaperArr,
+  //     arePapersFiltered: true,
+  //   };
+  // }
+
+  // if (type === "FILTER_PAPERS_BY_COURSE_YEAR") {
+  //   if (state.papersFiltered.length < 1) {
+  //     return {
+  //       ...state,
+  //       showAlert: true,
+  //       alertMsg: "Select the course first!",
+  //     };
+  //   } else {
+  //     const filteredPaperArr = state.papersFiltered.filter((paper) =>
+  //       payload === "" ? paper : paper.courseYear === payload
+  //     );
+  //     return {
+  //       ...state,
+  //       papersFiltered: filteredPaperArr,
+  //       arePapersFiltered: true,
+  //     };
+  //   }
+  // }
+  // if (type === "FILTER_PAPERS_BY_COURSE_YEAR") {
+  //   if (state.papersFiltered.length < 1) {
+  //     return {
+  //       ...state,
+  //       showAlert: true,
+  //       alertMsg: "Select the course first!",
+  //     };
+  //   } else {
+  //     const filteredPaperArr = state.papersFiltered.filter((paper) =>
+  //       payload === "" ? paper : paper.courseYear === payload
+  //     );
+  //     return {
+  //       ...state,
+  //       papersFiltered: filteredPaperArr,
+  //       arePapersFiltered: true,
+  //     };
+  //   }
+  // }
 
   if (type === "CLEAR_PAPER_FILTERS") {
     return {
@@ -164,6 +172,13 @@ export const reducer = (state, action) => {
     return {
       ...state,
       showAlert: false,
+    };
+  }
+
+  if (type === "HIDE_FILTER_SIDEBAR") {
+    return {
+      ...state,
+      showFilterSidebar: payload,
     };
   }
 };
