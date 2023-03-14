0c815fa0186446896da2a344cb5db09d70f096da
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2023-03-14 12:13:50 +0530

    minor loading page bug fixed

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { GlobalContext } from "../context/Context";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { GlobalContext } from '../context/Context';
 
 const Error = ({ msg }) => {
-  const { state, dispatch } = GlobalContext;
+  const { dispatch } = GlobalContext;
   return (
     <section className="error-page">
-      <h1>{msg ? msg : "Error 404! Module not found"}</h1>
+      <h1>{msg ? msg : 'Error 404! Module not found'}</h1>
       <Link
         to="/"
         className="btn error-page-btn"
         onClick={() =>
           dispatch({
-            type: "THROW_ERROR",
+            type: 'THROW_ERROR',
             payload: {
               throwError: false,
-              errorMsg: "",
+              errorMsg: '',
             },
           })
         }
